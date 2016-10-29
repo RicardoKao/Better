49d2a3a3ed60f7b3cb41d1afc1e180657e38953b
GppxiaoT <906528226@qq.com>
2016-10-30 03:55:06 +0800

    add tablayout

--- app/src/main/java/aiinno/com/better/ui/BaseActivity.java
+++ app/src/main/java/aiinno/com/better/ui/BaseActivity.java
@@ -2,14 +2,27 @@ package aiinno.com.better.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 
 import aiinno.com.better.R;
 
 public class BaseActivity extends AppCompatActivity {
+    private SimpleFragmentPagerAdapter pagerAdapter;
 
+    private ViewPager viewPager;
+
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        pagerAdapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager(), this);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
     }
 }
