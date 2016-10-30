f8e83f84415479694e1c35013daf10fbc0d459dc
GppxiaoT <906528226@qq.com>
2016-10-31 01:41:19 +0800

    add cards

--- app/src/main/java/layout/HomeFragment.java
+++ app/src/main/java/layout/HomeFragment.java
@@ -8,14 +8,26 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import aiinno.com.better.R;
+
+import com.dexafree.materialList.card.Card;
+import com.dexafree.materialList.card.CardProvider;
+import com.dexafree.materialList.card.OnActionClickListener;
+import com.dexafree.materialList.card.action.TextViewAction;
+import com.dexafree.materialList.card.action.WelcomeButtonAction;
+import com.dexafree.materialList.card.provider.ListCardProvider;
+import com.dexafree.materialList.listeners.OnDismissCallback;
+import com.dexafree.materialList.listeners.RecyclerItemClickListener;
+import com.dexafree.materialList.view.MaterialListView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
     SwipeRefreshLayout swipeLayout;
+    private MaterialListView mListView;
 
 
     public HomeFragment() {
@@ -37,6 +49,33 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
                 getResources().getColor(android.R.color.holo_blue_dark),
                 getResources().getColor(android.R.color.holo_orange_dark));
         //return inflater.inflate(R.layout.fragment_home, container, false);
+        Card card = new Card.Builder(getActivity())
+                .withProvider(new CardProvider())
+                .setLayout(R.layout.material_basic_buttons_card)
+                .setTitle("Card number 4")
+                .setDescription("Lorem ipsum dolor sit amet")
+                .addAction(R.id.left_text_button, new TextViewAction(getActivity())
+                        .setText("Izquierda")
+                        .setTextResourceColor(R.color.black_button)
+                        .setListener(new OnActionClickListener() {
+                            @Override
+                            public void onActionClicked(View view, Card card) {
+                                Toast.makeText(getActivity(), "You have pressed the left button", Toast.LENGTH_SHORT).show();
+                            }
+                        }))
+                .addAction(R.id.right_text_button, new TextViewAction(getActivity())
+                        .setText("Derecha")
+                        .setTextResourceColor(R.color.accent_material_dark)
+                        .setListener(new OnActionClickListener() {
+                            @Override
+                            public void onActionClicked(View view, Card card) {
+                                Toast.makeText(getActivity(), "You have pressed the right button", Toast.LENGTH_SHORT).show();
+                            }
+                        }))
+                .endConfig()
+                .build();
+        mListView = (MaterialListView) view.findViewById(R.id.material_listview);
+        mListView.getAdapter().addAll(card);
         return view;
     }
     @Override
