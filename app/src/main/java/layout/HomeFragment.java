25585bcee2e90cd8328a7794415b0a52239f61e1
GppxiaoT <906528226@qq.com>
2016-10-30 18:31:17 +0800

    add fresh

--- app/src/main/java/layout/HomeFragment.java
+++ app/src/main/java/layout/HomeFragment.java
@@ -3,6 +3,8 @@ package layout;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +14,8 @@ import aiinno.com.better.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+    SwipeRefreshLayout swipeLayout;
 
 
     public HomeFragment() {
@@ -24,7 +27,20 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_container);
+        swipeLayout.setOnRefreshListener(this);
+        swipeLayout.setColorSchemeColors(getResources().getColor(android.R.color.holo_green_dark),
+                getResources().getColor(android.R.color.holo_red_dark),
+                getResources().getColor(android.R.color.holo_blue_dark),
+                getResources().getColor(android.R.color.holo_orange_dark));
+        //return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+    @Override
+    public void onRefresh() {
+        Log.d("refresh","refreshing");
+    }
 }
